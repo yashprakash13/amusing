93b6dd9000916f03ac5069c96eb5a89b802d0fd2
Yash Prakash <yashprakash13@gmail.com>
2024-01-01 11:31:33 -0500

    fixed config file updates.

--- amusing/utils/config.py
+++ amusing/utils/config.py
@@ -1,5 +1,34 @@
+import os
+from pathlib import Path
+
 import yaml
 
 APP_CONFIG = {}
-with open("appconfig.yaml", "r") as file:
+
+
+def find_or_create_config_file():
+    # Define the file path
+    os.makedirs(os.path.join(Path.home(), "Downloads", "Amusing"))
+    file_path = os.path.join(Path.home(), "Downloads", "Amusing", "appconfig.yaml")
+
+    # Check if the file exists
+    if os.path.exists(file_path):
+        print(f"Found existing file: {file_path}")
+        return file_path
+    else:
+        # Create the file
+        with open(file_path, "w") as file:
+            # Write default configuration (you can customize this)
+            config_data = {
+                "root_download_path": str(
+                    os.path.join(Path.home(), "Downloads", "Amusing", "data")
+                ),
+                "db_name": "library.db",
+            }
+            yaml.dump(config_data, file)
+            print(f"Created a new config file: {file_path}")
+        return file_path
+
+
+with open(find_or_create_config_file(), "r") as file:
     APP_CONFIG = yaml.safe_load(file)
